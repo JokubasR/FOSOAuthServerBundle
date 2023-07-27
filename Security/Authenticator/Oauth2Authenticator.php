2df81c205d082f5f9712208de3d3dd8b453ee3a4
Jokubas Ramanauskas <jokubas.rama@gmail.com>
2023-07-27 16:50:24 +0300

    Use User::getUserIdentifier instead of a client\'s one

--- Security/Authenticator/Oauth2Authenticator.php
+++ Security/Authenticator/Oauth2Authenticator.php
@@ -58,7 +58,6 @@ class Oauth2Authenticator extends AbstractAuthenticator
             $accessToken = $this->serverService->verifyAccessToken($tokenString);
 
             $user = $accessToken->getUser();
-            $client = $accessToken->getClient();
 
             if (null !== $user) {
                 try {
@@ -87,7 +86,7 @@ class Oauth2Authenticator extends AbstractAuthenticator
 
             $accessTokenBadge = new AccessTokenBadge( $accessToken, $roles );
 
-            return new SelfValidatingPassport( new UserBadge( $client->getUserIdentifier() ), [ $accessTokenBadge ] );
+            return new SelfValidatingPassport( new UserBadge( $user->getUserIdentifier() ), [ $accessTokenBadge ] );
         } catch (OAuth2ServerException $e) {
             throw new AuthenticationException('OAuth2 authentication failed', 0, $e);
         }
